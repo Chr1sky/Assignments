27046c668d7fffeb758cff39c47d0451fa432b79
Christopher <110299226+Chr1sky@users.noreply.github.com>
2024-02-05 21:38:28 +0100

    Update and rename Convert a Number to a String! to Convert_a_Number_to_a_String.cpp

--- Code Wars/8 kyu/Convert_a_Number_to_a_String.cpp
+++ Code Wars/8 kyu/Convert_a_Number_to_a_String.cpp
@@ -1,4 +1,4 @@
-We need a function that can transform a number (integer) into a string.
+//We need a function that can transform a number (integer) into a string.
 
 // What ways of achieving this do you know?
 
